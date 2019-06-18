da4bcb2cfe40e022d27ad156aacfc3b440ed265e
Kevin Turcumani <Kevin.Turcumani@studytafensw.edu.au>
2019-06-18 13:49:01 +1000

    Initial commit

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Agent : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    public void GoHere(Vector3 position)
+    {
+        agent.SetDestination(position);
+    }
+}
---
+++ Assets/Scripts/Director.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Director : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If left mouse is down
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Perform raycast using camera
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(camRay, out hit))
+            {
+                // Get agent to go to hit point
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
